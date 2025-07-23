33432357ec7aea01bf55d95ae1015c4788cb8795
Kyle Nicolaysen <kylenicolaysen@gmail.com>
2025-07-22 22:24:21 -0700

    form and style cleanup

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,30 @@ const router = express.Router()
 
 // Main page
 router.get('/', (req, res) => {
-  res.render('checkin-form')
+    res.render('checkin-form')
 })
 
 // Form response
 router.post('/create-checkin', (req, res) => {
-  const { location } = req.session
-  const { return_time, emergency_contact_email, checkin_user_email } = req.body
+    const { location } = req.session
+    const { timezone, local_time, destination, return_time, emergency_contact_email, checkin_user_email } = req.body
+    console.log(location)
+    const result = db.prepare(`
+        INSERT INTO checkins (location, return_time, emergency_contact_email, checkin_user_email, created_at)
+        VALUES (?, ?, ?, ?, datetime('now'))
+    `).run(location, return_time, emergency_contact_email, checkin_user_email)
 
-  const result = db.prepare(`
-    INSERT INTO checkins (location, return_time, emergency_contact_email, checkin_user_email, created_at)
-    VALUES (?, ?, ?, ?, datetime('now'))
-  `).run(location, return_time, emergency_contact_email, checkin_user_email)
+    const checkinId = result.lastInsertRowid
 
-  const checkinId = result.lastInsertRowid
+    const schedule = require('../scheduler')
+    schedule(checkinId, return_time, emergency_contact_email, checkin_user_email, location)
 
-  const schedule = require('../scheduler')
-  schedule(checkinId, return_time, emergency_contact_email, checkin_user_email, location)
-
-  res.redirect(`/checkin/${checkinId}`)
+    res.redirect(`/checkin/${checkinId}`)
 })
 
 // Checkin page
 router.get('/checkin/:id', (req, res) => {
-  res.render('checkin', { id: req.params.id })
+    res.render('checkin', { id: req.params.id })
 })
 
 // Confirmation page
